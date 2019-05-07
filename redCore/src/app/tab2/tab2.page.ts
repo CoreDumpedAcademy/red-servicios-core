5ab936601cd5b9bbcbf9c68a1ed8e80715635576
Carlos <pochuelo15@gmail.com>
2019-05-07 22:39:08 +0200

    Rediseño de Home
    He puesto un calendario que de momento es una imagen, pero será un calendario de eventos. Las noticias ahora van por categorías, y hay un botón que lleva a la página de servicios

--- redCore/src/app/tab2/tab2.page.ts
+++ redCore/src/app/tab2/tab2.page.ts
@@ -8,31 +8,47 @@ import { Router } from '@angular/router';
 })
 export class Tab2Page {
 
-    public contents: Array<object> = [];
+    sliderConfig = {
+        loop: false,
+        initialSlide: 0,
+        spaceBetween: 5,
+        centeredSlides: true,
+        slidesPerView: 1.2
+      }
+
+    public contents: [{
+        img: String,
+        title: String,
+        msg: String
+    }]
 
     constructor(private router: Router) {
-        this.contents = [
-            {
-                img : '/assets/MicrosoftF82.jpg',
-                title : 'Tecnologías de Microsoft',
-                // tslint:disable-next-line:max-line-length
-                msg : 'El lunes 29 de abril a las 16:00h en el aula 3006 del Campus Sur de la UPM. Evento gratuito ¿Quieres aprender sobre cómo Microsoft hace lo que…'
-            },
-            {
-                img : '/assets/photo_2019-04-04.jpg',
-                title : 'La Curiosidad No Mató Al Gato, Ep. 7: Más pesados que el agujero negro de Messier 87',
-                msg : '¡Escúchalo en iVoox y suscríbete!'
-            },
-            {
-                img : '/assets/photo65972961570564334.jpg',
-                title : 'Carta del presidente',
-                msg : 'Sobre la filosofía de la asociación “Core Dumped es un espacio joven, ' +
-                    'abierto y gratuito que incentiva el aprendizaje y el desarrollo de proyectos más allá de lo…'
-            },
-        ];
+        this.contents = [{
+            img : '/assets/MicrosoftF82.jpg',
+            title : 'Tecnologías de Microsoft',
+            // tslint:disable-next-line:max-line-length
+            msg : 'El lunes 29 de abril a las 16:00h en el aula 3006 del Campus Sur de la UPM. Evento gratuito ¿Quieres aprender sobre cómo Microsoft hace lo que…'
+            }
+        ]
+        this.contents.push({
+            img : '/assets/photo_2019-04-04.jpg',
+            title : 'La Curiosidad No Mató Al Gato, Ep. 7: Más pesados que el agujero negro de Messier 87',
+            msg : '¡Escúchalo en iVoox y suscríbete!'
+        })
+        this.contents.push(
+        {
+            img : '/assets/photo65972961570564334.jpg',
+            title : 'Carta del presidente',
+            msg : 'Sobre la filosofía de la asociación “Core Dumped es un espacio joven, ' +
+                'abierto y gratuito que incentiva el aprendizaje y el desarrollo de proyectos más allá de lo…'
+        })
     }
 
     toAbout() {
         this.router.navigateByUrl('about')
+    }
+
+    gotoServices(){
+        this.router.navigateByUrl('servicios')
     }
 }
