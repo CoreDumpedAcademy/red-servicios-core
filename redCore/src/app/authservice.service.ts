2c79a2844b52c4d8748cb10733dcc82dbce421e9
mrmkrtm <m.kourtam@alumnos.upm.es>
2019-05-13 13:57:13 +0200

    Update

--- redCore/src/app/authservice.service.ts
+++ redCore/src/app/authservice.service.ts
@@ -26,7 +26,7 @@ export class AuthserviceService {
   
   //Registrar usuario
   addUser(user){
-    this.http.post(`${this.AUTH_SERVER_ADRESS}/`,{
+    this.http.post(`${this.AUTH_SERVER_ADRESS}/register`,{
       picture: user.img,
       name: user.firstName,
       password: user.password,
